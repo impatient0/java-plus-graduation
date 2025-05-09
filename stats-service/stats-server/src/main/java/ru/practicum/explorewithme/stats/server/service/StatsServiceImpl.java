4bb57c2c8418f6444fc68eac0282f856bf827856
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-05-10 00:38:41 +0300

    create StatsService interface and a stub implementation

---
+++ stats-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/service/StatsServiceImpl.java
@@ -0,0 +1,33 @@
+package ru.practicum.explorewithme.stats.server.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.practicum.explorewithme.stats.dto.EndpointHitDto;
+import ru.practicum.explorewithme.stats.dto.ViewStatsDto;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import ru.practicum.explorewithme.stats.server.repository.StatsRepository;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@SuppressWarnings("unused")
+public class StatsServiceImpl implements StatsService {
+
+    private final StatsRepository statsRepository;
+
+    @Override
+    public void saveHit(EndpointHitDto endpointHitDto) {
+        log.warn("STUB IMPLEMENTATION: StatsServiceImpl.saveHit called with DTO: {}", endpointHitDto);
+    }
+
+    @Override
+    public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
+        log.warn("STUB IMPLEMENTATION: StatsServiceImpl.getStats called with params: start={}, end={}, uris={}, unique={}",
+            start, end, uris, unique);
+        return Collections.emptyList();
+    }
+}
