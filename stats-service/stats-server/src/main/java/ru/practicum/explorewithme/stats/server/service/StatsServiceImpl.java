9586222a8d4bf19fc1a79b389694c4d59691778c
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-05-10 16:55:54 +0300

    annotate service methods as transactional

--- stats-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/service/StatsServiceImpl.java
+++ stats-service/stats-server/src/main/java/ru/practicum/explorewithme/stats/server/service/StatsServiceImpl.java
@@ -3,6 +3,7 @@ package ru.practicum.explorewithme.stats.server.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.explorewithme.stats.dto.EndpointHitDto;
 import ru.practicum.explorewithme.stats.dto.ViewStatsDto;
 
@@ -20,11 +21,13 @@ public class StatsServiceImpl implements StatsService {
     private final StatsRepository statsRepository;
 
     @Override
+    @Transactional
     public void saveHit(EndpointHitDto endpointHitDto) {
         log.warn("STUB IMPLEMENTATION: StatsServiceImpl.saveHit called with DTO: {}", endpointHitDto);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique) {
         log.warn("STUB IMPLEMENTATION: StatsServiceImpl.getStats called with params: start={}, end={}, uris={}, unique={}",
             start, end, uris, unique);
