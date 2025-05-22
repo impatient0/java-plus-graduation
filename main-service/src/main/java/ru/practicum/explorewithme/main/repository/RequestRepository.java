cafd20a94af6f0bfca8299f0c9f1dd5146c0f818
Sergey Filippovskikh <116564864+SergikF@users.noreply.github.com>
2025-05-22 12:09:02 +0300

    Исправление ошибок стиля

--- main-service/src/main/java/ru/practicum/explorewithme/main/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/explorewithme/main/repository/RequestRepository.java
@@ -2,7 +2,6 @@ package ru.practicum.explorewithme.main.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.practicum.explorewithme.main.dto.ParticipationRequestDto;
 import ru.practicum.explorewithme.main.model.ParticipationRequest;
 import ru.practicum.explorewithme.main.model.RequestStatus;
 
