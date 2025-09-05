2f0ccf5370a89cf748b1ea12781c7c8e5d2f7044
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-09-06 00:59:39 +0300

    feat(discovery): make main service and stats server register with Eureka

--- core/main-service/src/main/java/ru/practicum/explorewithme/main/MainServiceApplication.java
+++ core/main-service/src/main/java/ru/practicum/explorewithme/main/MainServiceApplication.java
@@ -2,10 +2,12 @@ package ru.practicum.explorewithme.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import ru.practicum.explorewithme.stats.client.config.StatsClientModuleConfiguration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import(StatsClientModuleConfiguration.class)
 public class MainServiceApplication {
     public static void main(String[] args) {
