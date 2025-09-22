014b92d90b053c8574ed8cd82a0e55c116ab56d9
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-09-22 22:57:16 +0300

    feat(serialization): add Avro/Protobuf schemas and generation
    - Define Avro and Protobuf schemas for core data models.
    - Implement custom Avro serializers and deserializers (ser/de).
    - Configure Maven plugins to auto-generate Java classes from schemas.

---
+++ core/serialization/src/main/java/ru/practicum/ewm/stats/common/kafka/AvroSerializer.java
@@ -0,0 +1,45 @@
+package ru.practicum.ewm.stats.common.kafka;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.BinaryEncoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+@Slf4j
+public class AvroSerializer<T extends SpecificRecordBase> implements Serializer<T> {
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        // No-op
+    }
+
+    @Override
+    public byte[] serialize(String topic, T payload) {
+        if (payload == null) {
+            return null;
+        }
+
+        GenericDatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(payload.getSchema());
+        try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+            BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
+            datumWriter.write(payload, encoder);
+            encoder.flush();
+            return outputStream.toByteArray();
+        } catch (IOException e) {
+            log.error("Error serializing Avro message for topic {}: {}", topic, payload, e);
+            throw new SerializationException("Error serializing Avro message", e);
+        }
+    }
+
+    @Override
+    public void close() {
+        // No-op
+    }
+}
