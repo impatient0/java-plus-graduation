ef217c73550d7214af61291dd69f17ea4f8e1d6e
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-09-23 16:14:51 +0300

    refactor(collector): decouple Kafka producer from controller
    - Extract Kafka publishing logic into a dedicated `UserActionProducer`.
    - Improves separation of concerns, leaving the controller focused on the gRPC layer.

--- core/collector/src/main/java/ru/practicum/ewm/collector/presentation/UserActionControllerImpl.java
+++ core/collector/src/main/java/ru/practicum/ewm/collector/presentation/UserActionControllerImpl.java
@@ -2,10 +2,10 @@ package ru.practicum.ewm.collector.presentation;
 
 import com.google.protobuf.Empty;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.kafka.core.KafkaTemplate;
+import ru.practicum.ewm.collector.infrastructure.kafka.UserActionProducer;
 import ru.practicum.ewm.collector.infrastructure.mapper.UserActionMapper;
 import ru.practicum.ewm.stats.grpc.UserActionControllerGrpc;
 import ru.practicum.ewm.stats.grpc.UserActionProto;
@@ -13,22 +13,11 @@ import ru.practicum.ewm.stats.kafka.UserActionAvro;
 
 @GrpcService
 @Slf4j
+@RequiredArgsConstructor
 public class UserActionControllerImpl extends UserActionControllerGrpc.UserActionControllerImplBase {
 
-    @Value("${kafka.topic.user-actions}")
-    private final String userActionsTopic;
-
-    private final KafkaTemplate<String, UserActionAvro> kafkaTemplate;
+    private final UserActionProducer producer;
     private final UserActionMapper mapper;
-
-    public UserActionControllerImpl(
-        @Value("${kafka.topic.user-actions}") String userActionsTopic,
-        KafkaTemplate<String, UserActionAvro> kafkaTemplate,
-        UserActionMapper mapper) {
-        this.userActionsTopic = userActionsTopic;
-        this.kafkaTemplate = kafkaTemplate;
-        this.mapper = mapper;
-    }
 
     @Override
     public void collectUserAction(UserActionProto request, StreamObserver<Empty> responseObserver) {
@@ -36,8 +25,7 @@ public class UserActionControllerImpl extends UserActionControllerGrpc.UserActio
 
         UserActionAvro avroMessage = mapper.toAvro(request);
 
-        kafkaTemplate.send(userActionsTopic, avroMessage);
-        log.info("Sent user action to Kafka topic '{}'", userActionsTopic);
+        producer.sendUserAction(avroMessage);
 
         responseObserver.onNext(Empty.getDefaultInstance());
         responseObserver.onCompleted();
